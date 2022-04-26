0b3e966424de4605f133a97c5815ff582bc8eb9c
Cait :) <99435089+mobleyc2@users.noreply.github.com>
2022-04-26 14:46:08 -0400

    Js formatting

--- java.js
+++ java.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', (event) =>{
-    var today = new Date();
-var time = today.getHours();
-var greet;
+  var today = new Date();
+  var time = today.getHours();
+  var greet;
 
-if (time > 18) {
-  greet = 'Good evening!';
-} else if (time > 12) {
-  greet = 'Good afternoon!';
-} else if (time >= 0) {
-  greet = 'Good Morning!';
-} else {
-  greet = 'Hi! I am Cait.';
-}
+  if (time > 18) {
+    greet = 'Good evening!';
+  } else if (time > 12) {
+    greet = 'Good afternoon!';
+  } else if (time >= 0) {
+    greet = 'Good Morning!';
+  } else {
+    greet = 'Hi! I am Cait.';
+  }
 
-var show = document.getElementById('message');
-show.textContent = greet;
+  var show = document.getElementById('message');
+  show.textContent = greet;
 }) 
